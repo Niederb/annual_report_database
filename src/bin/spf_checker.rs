08d29b0327d07783003830f3c471caf0a0dfee09
Thomas Niederberger <thomas@niederb.ch>
2022-04-01 10:15:58 +0200

    Refactor project structure

---
+++ src/bin/spf_checker.rs
@@ -0,0 +1,65 @@
+use decon_spf::Spf;
+use std::fs;
+use std::net::*;
+use trust_dns_resolver::config::*;
+use trust_dns_resolver::error::ResolveResult;
+use trust_dns_resolver::lookup::*;
+use trust_dns_resolver::Resolver;
+use viaspf_record::Record;
+
+use annual_report_database::data_structures::*;
+
+fn get_metadata() -> Vec<CompanyMetadata> {
+    let paths = fs::read_dir("metadata/").unwrap();
+    let mut metas = Vec::new();
+    for source_file in paths {
+        let path = source_file.unwrap().path();
+        let meta = CompanyMetadata::from_metadata(path.to_str().unwrap());
+        metas.push(meta);
+    }
+    metas
+}
+
+fn main() {
+    let mut resolver = Resolver::new(ResolverConfig::default(), ResolverOpts::default()).unwrap();
+    let metas = get_metadata();
+    for meta in metas {
+        let url = meta
+            .url
+            .replace("https://", "")
+            .replace("http://", "")
+            .replace("www.", "");
+        println!("{:?}, URL: {}", meta.name, meta.url);
+        spf_query(&mut resolver, &url);
+        println!();
+    }
+}
+
+fn spf_query(resolver: &mut Resolver, query: &str) {
+    let txt_response = resolver.txt_lookup(query);
+    let spf_record = display_txt(&query, &txt_response);
+    println!("Valid: {}, {:?}", spf_record.is_valid(), spf_record);
+}
+
+fn display_txt(query: &str, txt_response: &ResolveResult<TxtLookup>) -> Spf {
+    //let mut spf_record = "".to_string();
+    let mut spf_record = Spf::default();
+    match txt_response {
+        Err(_) => println!("No TXT Records."),
+        Ok(txt_response) => {
+            let mut i = 1;
+            //println!("List of TXT records found for {}", &query);
+            for record in txt_response.iter() {
+                //println!("TXT Record {}:", i);
+                //println!("{}", &record.to_string());
+                if record.to_string().starts_with("v=spf1") {
+                    spf_record = record.to_string().parse().unwrap_or(Spf::default());
+                    let a = record.to_string().parse::<Record>().unwrap();
+                    println!("{}", a);
+                }
+                i = i + 1;
+            }
+        }
+    }
+    spf_record
+}
---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+pub mod data_structures;
+pub mod extraction;
+pub mod reporting;
